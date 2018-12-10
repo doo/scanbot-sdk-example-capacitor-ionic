2c4b611a920de4da1f826476e0a8b3ba0fec932c
Eduard Frank <eddy@scanbot.io>
2018-12-10 21:36:34 +0100

    Add example for Document Scanner

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, Platform } from 'ionic-angular';
-import ScanbotSDK from 'cordova-plugin-scanbot-sdk';
+import ScanbotSDK, { Page } from 'cordova-plugin-scanbot-sdk';
 
 @Component({
   selector: 'page-home',
@@ -9,6 +9,8 @@ import ScanbotSDK from 'cordova-plugin-scanbot-sdk';
 export class HomePage {
 
   private SBSDK = ScanbotSDK.promisify();
+
+  public pages: Page[] = [];
 
   constructor(public navCtrl: NavController, platform: Platform) {
     platform.ready().then(() => this.initScanbotSDK());
@@ -25,6 +27,31 @@ export class HomePage {
     }).catch((err) => {
       console.error(err);
     });
+  }
+
+  async startDocumentScanner() {
+    const result = await this.SBSDK.UI.startDocumentScanner({
+      uiConfigs: {
+        // Customize colors, text resources, behavior, etc..
+        cameraPreviewMode: 'FIT_IN',
+        orientationLockMode: 'PORTRAIT',
+        pageCounterButtonTitle: '%d Page(s)',
+        multiPageEnabled: true
+        // ...
+      }
+    });
+
+    if (result.status === 'CANCELED') {
+      // user has canceled the scanning operation
+      return;
+    }
+
+    // Get the scanned pages from result:
+    this.pages = result.pages;
+  }
+
+  convertFileUri(fileUri) {
+    return (<any>window).Ionic.WebView.convertFileSrc(fileUri);
   }
 
 }
