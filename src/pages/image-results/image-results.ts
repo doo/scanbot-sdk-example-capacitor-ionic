6dc8f79242da3b4ee122ef2586d4f0356daf8fa6
Eduard Frank <eddy@scanbot.io>
2018-12-14 15:52:05 +0100

    Add OCR example

--- src/pages/image-results/image-results.ts
+++ src/pages/image-results/image-results.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
-import { NavController } from "ionic-angular";
+import { ActionSheetButton, ActionSheetController, NavController } from "ionic-angular";
 
 import { Plugins } from '@capacitor/core';
 
@@ -20,6 +20,7 @@ export class ImageResultsPage {
   public pages: SanitizedPage[] = [];
 
   constructor(private navCtrl: NavController,
+              private actionSheetCtrl: ActionSheetController,
               private scanbot: ScanbotSdkProvider,
               private imageResultsProvider: ImageResultsProvider,
               private uiService: UiService
@@ -46,13 +47,60 @@ export class ImageResultsPage {
         images: this.pages.map(p => p.documentImageFileUri)
       });
 
-      //this.uiService.showAlert(result.pdfFileUri, "PDF created");
-      Share.share({url: result.pdfFileUri});
+      this.uiService.showAlert(result.pdfFileUri, "PDF created").then(() => {
+        Share.share({url: result.pdfFileUri});
+      });
     }
     finally {
       loading.dismiss();
     }
   }
+
+  async saveAsPdfWithOcr() {
+    if (!(await this.scanbot.checkLicense())) { return; }
+    if (!this.checkImages()) { return; }
+
+    const loading = this.uiService.createLoading('Performing OCR and creating PDF ...');
+    try {
+      loading.present();
+      const result = await this.scanbot.SDK.performOcr({
+        images: this.pages.map(p => p.documentImageFileUri),
+        languages: ['en'],
+        outputFormat: 'FULL_OCR_RESULT',
+      });
+
+      this.uiService.showAlert(result.pdfFileUri, "PDF with OCR created").then(() => {
+        Share.share({url: result.pdfFileUri});
+      });
+    }
+    catch(e) {
+      console.error('Unable to perform OCR.', e);
+      this.uiService.showAlert(e.message, "ERROR");
+    }
+    finally {
+      loading.dismiss();
+    }
+  }
+
+  showSaveSelection() {
+    const actionButtons: ActionSheetButton[] = [
+      {
+        text: 'Save as PDF',
+        handler: () => { this.saveAsPdf(); }
+      },
+      {
+        text: 'Save as PDF with OCR',
+        handler: () => { this.saveAsPdfWithOcr(); }
+      },
+    ];
+
+    const actionSheet = this.actionSheetCtrl.create({
+      title: 'Save Images as',
+      buttons: actionButtons
+    });
+    actionSheet.present();
+  }
+
 
   private checkImages(): boolean {
     if (this.pages.length > 0) {
