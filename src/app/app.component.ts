efc0c08ee4937de99595b8ca6af1f2ea8839b1db
Marko <marko@scanbot.io>
2023-12-11 17:36:23 +0100

    Create licenseKey readonly property and add comment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,16 +16,29 @@ import { ScanbotSDK, ScanbotSdkConfiguration } from 'capacitor-plugin-scanbot-sd
 })
 export class AppComponent implements OnInit {
 
-    // !! Please read note !!
-    // It is strongly recommended to use the default (secure) storage location of the Scanbot SDK.
-    // However, for demo purposes we overwrite the "storageBaseDirectory" of the Scanbot SDK by a custom storage directory.
-    //
-    // For more details about the storage system of the Scanbot SDK Capacitor Module please see our docs:
-    // - https://docs.scanbot.io/document-scanner-sdk/capacitor/introduction/
-    //
-    // For more details about the file system on Android and iOS we also recommend to check out:
-    // - https://developer.android.com/training/data-storage
-    // - https://developer.apple.com/documentation/foundation/filemanager
+    /*
+     * TODO add the license key here.
+     * Please note: The Scanbot SDK will run without a license key for one minute per session!
+     * After the trial period has expired, all SDK functions and UI components will stop working.
+     * You can get a free "no-strings-attached" trial license.
+     * Please submit the trial license form (https://scanbot.io/trial/) on our website using
+     * the app identifier "io.scanbot.example.sdk.capacitor" of this example app
+     * or of your app (see capacitor.config.ts).
+     */
+    readonly licenseKey = '';
+
+    /*
+     * !! Please read note !!
+     * It is strongly recommended to use the default (secure) storage location of the Scanbot SDK.
+     * However, for demo purposes we overwrite the "storageBaseDirectory" of the Scanbot SDK by a custom storage directory.
+     * 
+     * For more details about the storage system of the Scanbot SDK Capacitor Module please see our docs:
+     * - https://docs.scanbot.io/document-scanner-sdk/capacitor/introduction/
+     * 
+     * For more details about the file system on Android and iOS we also recommend to check out:
+     * - https://developer.android.com/training/data-storage
+     * - https://developer.apple.com/documentation/foundation/filemanager
+     */
     readonly storageBaseDirectoryUri = Filesystem.getUri({
         path: 'my-custom-storage',
         directory: Directory.External,
@@ -44,7 +57,7 @@ export class AppComponent implements OnInit {
 
     private async initScanbotSdk() {
         const config: ScanbotSdkConfiguration = {
-            licenseKey: '',
+            licenseKey: this.licenseKey,
             loggingEnabled: true,
             storageImageFormat: 'JPG', // Format of stored images
             storageImageQuality: 80, // Quality of stored images
