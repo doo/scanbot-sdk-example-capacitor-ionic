90a3315c7537594f050bde2e7842d6e5af63b009
StefanKScanbot <stefankrzhovski@scanbot.io>
2025-07-28 09:20:53 +0200

    Added code snippets for VIN Scanner

---
+++ src/codeSnippets/vin/RTUUI/launchingTheScanner.ts
@@ -0,0 +1,15 @@
+import { startVINScanner, VinScannerScreenConfiguration } from 'capacitor-plugin-scanbot-sdk/ui_v2';
+
+async function startScanning() {
+  try {
+    /** Create an instance of the default configuration */
+    const configuration = new VinScannerScreenConfiguration();
+    /** Start the VIN Scanner UI */
+    const vinScannerResult = await startVINScanner(configuration);
+    /** Handle the result if the status is 'OK' */
+    if (vinScannerResult.status === 'OK') {
+    }
+  } catch (e: any) {
+    console.error(e.message);
+  }
+}
