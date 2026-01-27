c76c0a5f410e15679b88c76d98ec7f0819cb9226
Marko <marko@scanbot.io>
2026-01-27 16:31:59 +0100

    Update custom storage base directory path

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -37,7 +37,7 @@ export class AppComponent implements OnInit {
    */
   readonly storageBaseDirectoryUri = Filesystem.getUri({
     path: 'my-custom-storage',
-    directory: Directory.External,
+    directory: Directory.Data,
   });
 
   constructor() {}
